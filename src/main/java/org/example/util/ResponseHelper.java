aced7acf9ecc59594bee19166129c3981a0138f1
diogowa <martinho.diogom@gmail.com>
2026-03-24 02:12:08 +0000

    refactored

--- src/main/java/org/example/util/ResponseHelper.java
+++ src/main/java/org/example/util/ResponseHelper.java
@@ -17,16 +17,12 @@ public class ResponseHelper {
     public static final String FORBIDDEN = "The operation generated a forbidden error by other reason";
 
     public static Response ok(Map<String, Object> entity) {
-        return Response
-                .ok(Map.of("status", "success",
-                        "data", entity))
-                .build();
+        return Response.ok(Map.of("status", "success", "data", entity)).build();
     }
 
     public static Response error(String message) {
         String messageCode = getMessageCode(message);
-        return Response
-                .status(getHttpStatus(messageCode))
+        return Response.status(getHttpStatus(messageCode))
                 .entity(Map.of("status", messageCode, "data", message))
                 .build();
     }
