7fd5be1c13ce56566c9e461b057ec574078322b3
diogowa <martinho.diogom@gmail.com>
2026-03-29 22:06:39 +0100

    refactored Token

--- src/main/java/org/example/model/Token.java
+++ src/main/java/org/example/model/Token.java
@@ -2,10 +2,8 @@ package org.example.model;
 
 public class Token {
     public String tokenId;
-    public String username;
 
     public boolean isValid() {
-        return tokenId != null && !tokenId.isBlank()
-                && username != null && !username.isBlank();
+        return tokenId != null && !tokenId.isBlank();
     }
 }
