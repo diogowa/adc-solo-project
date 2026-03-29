395df809589da31a71973e640edf9de9d9c412e7
diogowa <martinho.diogom@gmail.com>
2026-03-30 00:47:25 +0100

    removed shell tests and implemented junit with assured

---
+++ src/test/java/CreateAccountTest.java
@@ -0,0 +1,315 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class CreateAccountTest {
+
+    @BeforeAll
+    static void setup() {
+        RestAssured.baseURI = "http://localhost:8080/rest";
+    }
+
+    @Test
+    void createUser_usernameIsNull() {
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "",
+                                "password": "pwd",
+                                "confirmation": "pwd",
+                                "phone": "1234",
+                                "address": "street",
+                                "role": "USER"
+                              }
+                            }
+                        """)
+                .when()
+                .post("/createaccount")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("9906"));
+    }
+
+    @Test
+    void createUser_invalidUsername() {
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "user1fct",
+                                "password": "pwd",
+                                "confirmation": "pwd",
+                                "phone": "1234",
+                                "address": "street",
+                                "role": "USER"
+                              }
+                            }
+                        """)
+                .when()
+                .post("/createaccount")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("9906"));
+    }
+
+    @Test
+    void createUser_passwordIsNull() {
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "user1@fct",
+                                "password": "",
+                                "confirmation": "pwd",
+                                "phone": "1234",
+                                "address": "street",
+                                "role": "USER"
+                              }
+                            }
+                        """)
+                .when()
+                .post("/createaccount")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("9906"));
+    }
+
+    @Test
+    void createUser_confirmationIsNull() {
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "user1@fct",
+                                "password": "pwd",
+                                "confirmation": "",
+                                "phone": "1234",
+                                "address": "street",
+                                "role": "USER"
+                              }
+                            }
+                        """)
+                .when()
+                .post("/createaccount")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("9906"));
+    }
+
+    @Test
+    void createUser_passwordAndConfirmationDoNotMatch() {
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "user1@fct",
+                                "password": "pwd",
+                                "confirmation": "asd",
+                                "phone": "1234",
+                                "address": "street",
+                                "role": "USER"
+                              }
+                            }
+                        """)
+                .when()
+                .post("/createaccount")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("9906"));
+    }
+
+    @Test
+    void createUser_phoneIsNull() {
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "user1@fct",
+                                "password": "pwd",
+                                "confirmation": "pwd",
+                                "phone": "",
+                                "address": "street",
+                                "role": "USER"
+                              }
+                            }
+                        """)
+                .when()
+                .post("/createaccount")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("9906"));
+    }
+
+    @Test
+    void createUser_invalidPhone_notEnoughDigits() {
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "user1@fct",
+                                "password": "pwd",
+                                "confirmation": "pwd",
+                                "phone": "12",
+                                "address": "street",
+                                "role": "USER"
+                              }
+                            }
+                        """)
+                .when()
+                .post("/createaccount")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("9906"));
+    }
+
+    @Test
+    void createUser_invalidPhone_moreDigits() {
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "user1@fct",
+                                "password": "pwd",
+                                "confirmation": "pwd",
+                                "phone": "1234567891234567",
+                                "address": "street",
+                                "role": "USER"
+                              }
+                            }
+                        """)
+                .when()
+                .post("/createaccount")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("9906"));
+    }
+
+    @Test
+    void createUser_invalidPhone_isNotNumber() {
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "user1@fct",
+                                "password": "pwd",
+                                "confirmation": "pwd",
+                                "phone": "abcdefg",
+                                "address": "street",
+                                "role": "USER"
+                              }
+                            }
+                        """)
+                .when()
+                .post("/createaccount")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("9906"));
+    }
+
+    @Test
+    void createUser_addressIsNull() {
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "user1@fct",
+                                "password": "pwd",
+                                "confirmation": "pwd",
+                                "phone": "1234",
+                                "address": "",
+                                "role": "USER"
+                              }
+                            }
+                        """)
+                .when()
+                .post("/createaccount")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("9906"));
+    }
+
+    @Test
+    void createUser_roleIsNull() {
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "user1@fct",
+                                "password": "pwd",
+                                "confirmation": "pwd",
+                                "phone": "1234",
+                                "address": "street",
+                                "role": ""
+                              }
+                            }
+                        """)
+                .when()
+                .post("/createaccount")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("9906"));
+    }
+
+    @Test
+    void createUser_invalidRole() {
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "user1@fct",
+                                "password": "pwd",
+                                "confirmation": "pwd",
+                                "phone": "1234",
+                                "address": "street",
+                                "role": "NONE"
+                              }
+                            }
+                        """)
+                .when()
+                .post("/createaccount")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("9906"));
+    }
+
+    @Test
+    void createUser_success() {
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "user1@fct",
+                                "password": "pwd",
+                                "confirmation": "pwd",
+                                "phone": "1234",
+                                "address": "street",
+                                "role": "USER"
+                              }
+                            }
+                        """)
+                .when()
+                .post("/createaccount")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("success"))
+                .body("data.username", equalTo("user1@fct"))
+                .body("data.role", equalTo("USER"));
+    }
+}
