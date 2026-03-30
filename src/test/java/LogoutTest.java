adca9e1e746449ca07aece4f53318a9cdf1e5039
diogowa <martinho.diogom@gmail.com>
2026-03-30 18:31:58 +0100

    test for logout

---
+++ src/test/java/LogoutTest.java
@@ -0,0 +1,416 @@
+import com.google.cloud.datastore.Datastore;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.*;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class LogoutTest {
+
+    private static String userTokenId;
+    private static String adminTokenId;
+    private static String bofficerTokenId;
+
+    @BeforeAll
+    static void setup() {
+        RestAssured.baseURI = "http://localhost:8080/rest";
+    }
+
+    @BeforeEach
+    void setupTest() throws Exception {
+        // clean database
+        URL url = new URL("http://localhost:8081/reset");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("POST");
+        con.getResponseCode();
+
+        // place users
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "user1@fct",
+                                "password": "pwd",
+                                "confirmation": "pwd",
+                                "phone": "1234",
+                                "address": "street",
+                                "role": "USER"
+                              }
+                            }
+                        """)
+                .when()
+                .post("/createaccount")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("success"));
+
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "admin1@fct",
+                                "password": "pwd",
+                                "confirmation": "pwd",
+                                "phone": "1234",
+                                "address": "street",
+                                "role": "ADMIN"
+                              }
+                            }
+                        """)
+                .when()
+                .post("/createaccount")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("success"));
+
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "bofficer1@fct",
+                                "password": "pwd",
+                                "confirmation": "pwd",
+                                "phone": "1234",
+                                "address": "street",
+                                "role": "BOFFICER"
+                              }
+                            }
+                        """)
+                .when()
+                .post("/createaccount")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("success"));
+
+        // login
+        userTokenId = given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "user1@fct",
+                                "password": "pwd"
+                              }
+                            }
+                        """)
+                .when()
+                .post("/login")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("success"))
+                .extract().path("data.token.tokenId");
+
+        adminTokenId = given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "admin1@fct",
+                                "password": "pwd"
+                              }
+                            }
+                        """)
+                .when()
+                .post("/login")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("success"))
+                .extract().path("data.token.tokenId");
+
+        bofficerTokenId = given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "bofficer1@fct",
+                                "password": "pwd"
+                              }
+                            }
+                        """)
+                .when()
+                .post("/login")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("success"))
+                .extract().path("data.token.tokenId");
+    }
+
+    @Test
+    void logout_byUser_forbidden() {
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "bofficer1@fct"
+                              },
+                              "token": {
+                                "tokenId": "%s"
+                              }
+                            }
+                        """.formatted(userTokenId))
+                .when()
+                .post("/logout")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("9907"));
+
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "admin1@fct"
+                              },
+                              "token": {
+                                "tokenId": "%s"
+                              }
+                            }
+                        """.formatted(userTokenId))
+                .when()
+                .post("/logout")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("9907"));
+    }
+
+    @Test
+    void logout_byBofficer_forbidden() {
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "user1@fct"
+                              },
+                              "token": {
+                                "tokenId": "%s"
+                              }
+                            }
+                        """.formatted(bofficerTokenId))
+                .when()
+                .post("/logout")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("9907"));
+
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "admin1@fct"
+                              },
+                              "token": {
+                                "tokenId": "%s"
+                              }
+                            }
+                        """.formatted(bofficerTokenId))
+                .when()
+                .post("/logout")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("9907"));
+    }
+
+    @Test
+    void logout_userAndBofficer_success() {
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "user1@fct"
+                              },
+                              "token": {
+                                "tokenId": "%s"
+                              }
+                            }
+                        """.formatted(userTokenId))
+                .when()
+                .post("/logout")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("success"));
+
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "bofficer1@fct"
+                              },
+                              "token": {
+                                "tokenId": "%s"
+                              }
+                            }
+                        """.formatted(bofficerTokenId))
+                .when()
+                .post("/logout")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("success"));
+
+        // test if tokens were deleted
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                              },
+                              "token": {
+                                "tokenId": "%s"
+                              }
+                            }
+                        """.formatted(adminTokenId))
+                .when()
+                .post("/showauthsessions")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("success"))
+                .body("data.sessions", hasSize(1))
+                .body("data.sessions.username", hasItems("admin1@fct"));
+    }
+
+    @Test
+    void logout_userAndBofficer_byAdmin_success() {
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "user1@fct"
+                              },
+                              "token": {
+                                "tokenId": "%s"
+                              }
+                            }
+                        """.formatted(adminTokenId))
+                .when()
+                .post("/logout")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("success"));
+
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "bofficer1@fct"
+                              },
+                              "token": {
+                                "tokenId": "%s"
+                              }
+                            }
+                        """.formatted(adminTokenId))
+                .when()
+                .post("/logout")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("success"));
+
+        // test if tokens were deleted
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                              },
+                              "token": {
+                                "tokenId": "%s"
+                              }
+                            }
+                        """.formatted(adminTokenId))
+                .when()
+                .post("/showauthsessions")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("success"))
+                .body("data.sessions", hasSize(1))
+                .body("data.sessions.username", hasItems("admin1@fct"));
+
+        // test if users still exist
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                              },
+                              "token": {
+                                "tokenId": "%s"
+                              }
+                            }
+                        """.formatted(adminTokenId))
+                .when()
+                .post("/showusers")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("success"))
+                .body("data.users", hasSize(3));
+    }
+
+    @Test
+    void logout_byAdmin_toHimself_success() {
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                                "username": "admin1@fct"
+                              },
+                              "token": {
+                                "tokenId": "%s"
+                              }
+                            }
+                        """.formatted(adminTokenId))
+                .when()
+                .post("/logout")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("success"));
+
+        // test if token was deleted
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                              },
+                              "token": {
+                                "tokenId": "%s"
+                              }
+                            }
+                        """.formatted(adminTokenId))
+                .when()
+                .post("/showauthsessions")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("9903"));
+
+        // test if admin still exists
+        given()
+                .contentType("application/json")
+                .body("""
+                            {
+                              "input": {
+                              },
+                              "token": {
+                                "tokenId": "%s"
+                              }
+                            }
+                        """.formatted(bofficerTokenId))
+                .when()
+                .post("/showusers")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("success"))
+                .body("data.users", hasSize(3));
+    }
+}
